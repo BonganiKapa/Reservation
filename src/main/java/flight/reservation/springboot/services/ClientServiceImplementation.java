bfdbefd72b37efa23b007223fe9985e9510dc1de
Bongani <kapabongani123@gmail.com>
2020-05-13 15:10:44 +0200

    3rd Commit

--- src/main/java/flight/reservation/springboot/services/ClientServiceImplementation.java
+++ src/main/java/flight/reservation/springboot/services/ClientServiceImplementation.java
@@ -1,10 +1,13 @@
 package flight.reservation.springboot.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import flight.reservation.springboot.exception.ResourceNotFoundException;
 import flight.reservation.springboot.model.PersonalDetails;
 import flight.reservation.springboot.repository.ClientRepository;
 
@@ -12,30 +15,57 @@ import flight.reservation.springboot.repository.ClientRepository;
 @Transactional
 public class ClientServiceImplementation implements ClientServices {
 
+	@Autowired
 	private ClientRepository cr;
+	
+	@Override
 	public PersonalDetails createClient(PersonalDetails client) {
-		// TODO Auto-generated method stub
-		return null;
+		return cr.save(client);
 	}
 
+	@Override
 	public PersonalDetails updateClient(PersonalDetails client) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional <PersonalDetails> clientDB = this.cr.findById(client.getId());
+		
+		if(clientDB.isPresent()) {
+			PersonalDetails clientUpdate = clientDB.get();
+			clientUpdate.setId(client.getId());
+			clientUpdate.setName(client.getName());
+			clientUpdate.setSurname(client.getSurname());
+			cr.save(clientUpdate);
+			return clientUpdate;
+		}
+		else {
+			throw new ResourceNotFoundException("Record Not Found with ID : " + client.getId());
+		}
+		
 	}
 
 	public List<PersonalDetails> getClientList() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return this.cr.findAll();
 	}
 
 	public PersonalDetails getClientByID(long clientId) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Optional <PersonalDetails> clientDB = this.cr.findById(clientId);
+		
+		if(clientDB.isPresent()) {
+			return clientDB.get();
+		}
+		else {
+			throw new ResourceNotFoundException("Record Not Found with ID : " + clientId);
+		}
+		
 	}
 
 	public void deleteClient(long id) {
-		// TODO Auto-generated method stub
 		
+		Optional <PersonalDetails> clientDB = this.cr.findById(id);
+		
+		if(clientDB.isPresent()) {
+			this.cr.delete(clientDB.get());
+		}
 	}
 
 }
